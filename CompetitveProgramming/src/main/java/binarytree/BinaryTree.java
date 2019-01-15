06a1aa2b92744dfbdab080efb2e5af960e6fbb21
Shishir Dwivedi <shishir.dwivedi@payu.in>
2019-01-15 19:51:09 +0530

    FindMax and Search Element in Binary Tree.
    Signed-off-by: Shishir Dwivedi <shishir.dwivedi@payu.in>

--- CompetitveProgramming/src/main/java/binarytree/BinaryTree.java
+++ CompetitveProgramming/src/main/java/binarytree/BinaryTree.java
@@ -1,5 +1,8 @@
 package binarytree;
 
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
+import javax.swing.text.AbstractDocument.LeafElement;
+
 public class BinaryTree {
 
 	public   BTNode root;
@@ -17,6 +20,62 @@ public class BinaryTree {
 
 	}
 	
+	/**
+	 * Find Maximum Element in binary Tree
+	 * @param root
+	 * @return
+	 */
+	public int  findMaxElement(BTNode root) {
+		int max_left = 0;
+		int max_right = 0;
+		int max,root_val = 0;
+		if(root!=null) {
+			root_val=root.data;
+			max_left=findMaxElement(root.left);
+			max_right=findMaxElement(root.right);
+		}
+		if(max_left>max_right)
+			max=max_left;
+		else
+			max=max_right;
+		if(root_val>max)
+			return root_val;
+		else
+			return max;
+		
+		
+		
+	}
+	
+	/**
+	 * Search an Element in Binary tree
+	 * @param root
+	 * @param element
+	 * @return
+	 */
+	public boolean searchElement(BTNode root, int element) {
+		
+		if(root==null)
+			return false;
+		if(root.data==element)
+			return true;
+		else {
+			boolean found=searchElement(root.left, element);
+			if(found)
+				return true;
+			else {
+			return searchElement(root.right, element);
+			}
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 	public static void main(String[] args) {
 		BinaryTree tree=new BinaryTree(1);
@@ -26,6 +85,8 @@ public class BinaryTree {
 		tree.root.left.right=new BTNode(5);
 		tree.root.right.left=new BTNode(6);
 		tree.root.right.right=new BTNode(7);
-		tree.inorderTraversal(tree.root);
+		//tree.inorderTraversal(tree.root);
+		System.out.println(tree.findMaxElement(tree.root));
+		System.out.println(tree.searchElement(tree.root, 5));
 	}
 }
